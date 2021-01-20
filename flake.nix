8eb159fe59b502a9bc1f16f4330f79c0c49fe654
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2021-01-20 23:39:59 +0800

    [project] lots of modifications
    - create cabal.project to pin index state
    - remove cabal.project.freeze since it restrics the base version
    - start using haskell.nix
    - remove nixpkgs and using new one from haskell.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,40 @@
 {
   description = "A very basic flake";
 
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+  inputs = {
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, flake-utils, haskell-nix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = haskell-nix.legacyPackages.${system};
         hsPkgs = pkgs.haskellPackages;
 
-        too-many-logs = hsPkgs.callCabal2nix "too-many-logs" ./. { };
+        haskellNix = pkgs.haskell-nix.cabalProject {
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "too-many-logs-src";
+            src = ./.;
+          };
+          compiler-nix-name = "ghc8103";
+        };
+
+        too-many-logs = haskellNix.too-many-logs.components.exes.tml;
 
       in {
         packages.too-many-logs = too-many-logs;
 
-        devShell = hsPkgs.shellFor {
-          packages = p: [ too-many-logs ];
-          withHoogle = true;
-          nativeBuildInputs = [
-            hsPkgs.cabal-install
-            hsPkgs.hpack
-            hsPkgs.ghcid
-            hsPkgs.haskell-language-server
-          ];
+        devShell = haskellNix.shellFor {
+          packages = p: [ p.too-many-logs ];
+          withHoogle = false;
+          tools = {
+            cabal = "3.2.0.0";
+            haskell-language-server = "0.8.0";
+          };
+          nativeBuildInputs =
+            [ hsPkgs.hpack haskellNix.too-many-logs.project.roots ];
+          exactDeps = true;
         };
 
         defaultPackage = too-many-logs;
