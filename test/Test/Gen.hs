c5b3640786730099f4f1fcc8e7d153346430152d
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2021-02-07 16:01:53 +0800

    [test] impl Gens for dir/file

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Test.Gen
   ( genDay,
     genUTCTime,
+    absFile,
+    relDir,
+    absDir,
+    relFile,
   )
 where
 
@@ -8,6 +15,8 @@ import Data.Time (Day, UTCTime (UTCTime))
 import Data.Time.Calendar.OrdinalDate (fromOrdinalDate)
 import Hedgehog (Gen)
 import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Path
 
 -- | Gen Day with year of Gen.enum (-2020) 2020 and dayOfYear of Gen.enum 1 366.
 genDay :: Gen Day
@@ -25,3 +34,27 @@ genUTCTime = UTCTime <$> genDay <*> genDayTime
   where
     -- We ignore leap-seconds here due to its complexity.
     genDayTime = Gen.enum 0 86400
+
+relDir :: Gen (Path Rel Dir)
+relDir = do
+  p <- oneRelDir
+  ps <- Gen.list (Range.linear 1 50) oneRelDir
+  return $ foldl' (</>) p ps
+  where
+    oneRelDir = Gen.mapMaybe parseRelDir $ Gen.string (Range.linear 1 50) Gen.alphaNum
+
+absDir :: Gen (Path Abs Dir)
+absDir = do
+  rel <- relDir
+  return $ [absdir|/|] </> rel
+
+relFile :: Gen (Path Rel File)
+relFile = do
+  dir <- relDir
+  file <- Gen.mapMaybe parseRelFile $ Gen.string (Range.linear 1 50) Gen.alphaNum
+  return $ dir </> file
+
+absFile :: Gen (Path Abs File)
+absFile = do
+  rel <- relFile
+  return $ [absdir|/|] </> rel
