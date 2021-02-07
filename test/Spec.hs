b465685386e421bf76e0460e22f8d5f6e49f2bff
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2021-02-07 13:57:24 +0800

    [test] use defaultMain to replace handwritten one

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,11 @@
 
 module Main where
 
-import Control.Exception.Safe (throwString)
+import Hedgehog.Main (defaultMain)
 import qualified Test.TooManyLogs
 
 main :: IO ()
-main = do
-  testsPassed <-
-    sequenceA
-      [ Test.TooManyLogs.tests
-      ]
-  unless (and testsPassed) $
-    throwString "failed to run tests"
+main =
+  defaultMain
+    [ Test.TooManyLogs.tests
+    ]
